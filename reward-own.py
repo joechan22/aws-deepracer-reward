954ad0c9ea3d3f906891d1bfb16c69b999ffba0b
joechan22 <92969478+joechan22@users.noreply.github.com>
2022-11-05 13:02:49 +0800

    initial my reward function

---
+++ reward-own.py
@@ -0,0 +1,49 @@
+def reward_function(params):
+    """
+    params
+    {
+    "all_wheels_on_track": Boolean,        # flag to indicate if the agent is on the track
+    "x": float,                            # agent's x-coordinate in meters
+    "y": float,                            # agent's y-coordinate in meters
+    "closest_objects": [int, int],         # zero-based indices of the two closest objects to the agent's current position of (x, y).
+    "closest_waypoints": [int, int],       # indices of the two nearest waypoints.
+    "distance_from_center": float,         # distance in meters from the track center 
+    "is_crashed": Boolean,                 # Boolean flag to indicate whether the agent has crashed.
+    "is_left_of_center": Boolean,          # Flag to indicate if the agent is on the left side to the track center or not. 
+    "is_offtrack": Boolean,                # Boolean flag to indicate whether the agent has gone off track.
+    "is_reversed": Boolean,                # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
+    "heading": float,                      # agent's yaw in degrees
+    "objects_distance": [float, ],         # list of the objects' distances in meters between 0 and track_length in relation to the starting line.
+    "objects_heading": [float, ],          # list of the objects' headings in degrees between -180 and 180.
+    "objects_left_of_center": [Boolean, ], # list of Boolean flags indicating whether elements' objects are left of the center (True) or not (False).
+    "objects_location": [(float, float),], # list of object locations [(x,y), ...].
+    "objects_speed": [float, ],            # list of the objects' speeds in meters per second.
+    "progress": float,                     # percentage of track completed
+    "speed": float,                        # agent's speed in meters per second (m/s)
+    "steering_angle": float,               # agent's steering angle in degrees
+    "steps": int,                          # number steps completed
+    "track_length": float,                 # track length in meters.
+    "track_width": float,                  # width of the track
+    "waypoints": [(float, float), ]        # list of (x,y) as milestones along the track center
+    }
+    """
+
+    DEFAULT_REWARD = 1.0
+    LOWEST_REWARD = 1e-3
+    DIRECTION_THRESHOLD = 10.0
+    ABS_STEERING_THRESHOLD = 25.0
+    REINFORCE_FACTOR = 1.2
+    PUNISH_FACTOR_1 = 0.8
+    PUNISH_FACTOR_2 = 0.5
+
+    # parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    is_left_of_center = params['is_left_of_center']
+    all_wheels_on_track = params['all_wheels_on_track']
+    abs_steering = abs(params['steering_angle'])
+    speed = params['speed']
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints'] 
+    heading = params['heading']
+    progress = params['progress']
