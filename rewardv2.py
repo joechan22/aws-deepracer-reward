99ff16c66e1e6ddf19a5b2c6276c7af61ac32b27
joechan22 <92969478+joechan22@users.noreply.github.com>
2022-11-08 09:18:09 +0800

    finalize the code

--- rewardv2.py
+++ rewardv2.py
@@ -176,20 +176,6 @@ def reward_function(params):
         
     r = RewardClass()
     reward = DEFAULT_REWARD
-    # reward = r.chk_exception(reward, is_offtrack)
-    # reward = r.chk_on_track(reward, all_wheels_on_track)
-    # reward = r.chk_center_distance(reward, track_width, distance_from_center)
-    # reward = r.chk_straight_line(reward, abs_steering, speed)
-    # reward = r.chk_direction(reward, waypoints, closest_waypoints, heading)
-    # reward = r.chk_steering(reward, abs_steering)
-    # reward = r.chk_steering_rate(reward, speed, abs_steering)
-    # reward = r.chk_is_left_of_center(reward, is_left_of_center)
-    # reward = r.chk_progress(reward, progress)
-    # reward = r.chk_speed(reward, speed)
-    
-    # reward = r.chk_exception(reward, is_crashed)
-    # reward = r.chk_exception(reward, is_reversed)
-
     reward = r.chk_on_track(reward, all_wheels_on_track)
     reward = r.chk_exception(reward, is_offtrack)
     reward = r.chk_exception(reward, is_crashed)
